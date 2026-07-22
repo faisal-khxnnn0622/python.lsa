b386050d67ff30c8548740cb160547ee64934df9
faisalkhxnn0622 <faisal.khaan2008@gmail.com>
2026-07-22 20:42:32 +0530

    Add files via upload

--- complete.py
+++ complete.py
@@ -235,3 +235,298 @@ print(lst)
 
 lst.clear()
 print(lst)
+
+lst1 = [10 , 25 , 30 , 22 , 70 , 95]
+lst1.sort()
+print(lst1)
+lst1.sort(reverse=True)
+print(lst1)
+
+marks = 90 # Can also use int thn input
+if (marks>=95):
+    print("A GRADE")
+elif(marks>=70):
+    print("B GRADE")
+elif(marks>=55): 
+    print("C GRADE")
+else:
+    print("FAIL")
+
+
+
+# Functions :
+# It is reusable block of code
+
+
+def greet(): #FUnction
+    print("Welcome to python!") #function body
+greet()
+
+def greet(name):
+    print("Hello" , name)
+greet("FAISAL")
+
+# Multiple parameters
+def add(a,b):
+    print(a+b)
+add(10,20)
+
+# Return statement
+
+def ad(a,b):
+    return a+b
+result = ad(15,25)
+print(result)
+
+# Dictionaries
+
+info = {
+        "nameeee" : "Faisal",
+        "subjects" : ["python" , "c++" , "Java"],
+        "topics" : ("dictionaries" , "Sets"),
+        "age22" : 18,
+        "is_student" : True,
+        "marks" : 95.7,
+}
+print(info)
+print(type(info))
+print(info["nameeee"])
+print(info["subjects"])
+print(info["age22"])
+# We can make changes in dictionary
+info["nameeee"] = "Faisal"" ""khan"
+info["Nickname"] = "Sher" " " "khan"
+print(info)
+
+
+# Nested dictionaries
+
+student = {
+            "name11" : "FAISALEST KHAN",
+            "subjects" : {
+                    "maths" : 97,
+                    "chem" : 99,
+                    "phy" : 94,
+                
+            }   
+}
+
+print(student)
+[print(student["subjects"])]
+[print(student["subjects"]["maths"])]
+
+## Dict functions
+# .key()
+
+print((student.keys()))
+print(list(info.keys())) #Can also make dict keys to list
+
+# .values
+print(student.values())
+print(list(info.values()))
+
+# .items
+print(student.items())
+print(list(info.items()))
+
+# To create a list of only one item
+pair = list(student.items())
+print(pair[0])
+print(pair[1])
+
+pairs = list(info.items())
+print(pairs[4])
+
+# .get("key")
+
+print(student.get("name11"))
+print(student.get("age11")) #There is no key present like that so none value
+# print(student["age11"]) #This will show error thats why we use get.("key") as we write long long programs
+
+
+# .update
+new_dict = {"Grade" : 9}
+student.update(new_dict)
+print(student)
+# Or                               # Both will work
+
+student.update({"grade" : 9})
+print(student)
+
+
+# Sets 
+# -> Set is the collection of unordered value 
+# -> Each value in the set must be unique and immutable
+
+collection = {1 , 2 , 3 , 4 , "Hello world" , 2.2 ,}
+print(collection)
+print(type(collection))
+
+collection1 = {1 , 2 , 2 , 2 , 3 , 3 , 3 , "hello" , "hello"}
+print(collection1)
+
+collection3 = set()
+print(type(collection3))
+
+# Functions of sets
+
+# set.add()
+
+collection = set()
+collection.add(1)
+collection.add(2)
+collection.add(3)
+collection.add(4)
+collection.add("helololo")
+print(collection)
+
+# set.remove()
+collection.remove(1)
+collection.remove(2)
+print(collection)
+
+# set.pop()
+
+print(collection.pop()) # Prints any random value
+print(collection.pop())
+
+# set.clear()
+print(collection.clear())
+print(len(collection))
+
+# set.union
+set1 = {1 , 2 , 3, 4}
+set2 = {3 , 4 , 5 , 6}
+print(set1.union(set2))
+
+# set.intersection
+print(set1.intersection(set2))
+
+
+# While loops
+x =1
+while x<=10:
+    print(x)
+    x +=1
+a = 3  
+i = 1
+while i<=10:
+    print(f"{a} x {i} = {a*i}"  )
+    i += 1
+    
+count = 1
+while count<=3:
+    print(count , "Hello")
+    count +=1
+    
+l = 1
+while l<=100:
+    print(l)
+    l+=1
+    
+nums = [1 , 4 , 9 , 16 , 25 , 36 , 49 , 64 , 81 , 100]
+idx = 0
+while idx<len(nums):
+    print(nums[idx])
+    idx += 1
+    
+r = 1 
+while r <= 6:
+    print(r)
+    r += 1
+    if r==5:
+        break
+    
+print("Loop ended")
+
+i = 0
+while i <=7:
+    if i == 1:
+        i +=1
+    print(i)
+    i += 1
+
+
+# For loop   
+numss = [1 , 4 , 9 , 16 , 25 , 36 , 49 , 64 , 81 , 100]
+x = int(49)
+for num1 in numss:
+    if num1==x:
+        print("Found" , x )
+        break
+   
+    print(num1)
+    
+scores = [88 , 79 , 83 , 91 , 92 , 77 , 56]
+
+total = 0
+highest = 0
+lowest = 0
+average = 0
+toppers = 0
+for score in scores:
+    total+=score
+    if score>highest:
+        highest = score
+    if score<lowest:
+        lowest = score
+    if score>=90:
+        toppers+=1
+    
+average = total / len(scores)
+
+
+print("TOTAL - " , total)
+print("HIGHEST MARKS - " , highest)
+print("LOWEST MARKS - " , lowest)
+print("AVERAGE MARKS - " ,average)
+print("TOPPERS - " , toppers)
+
+#Enumerate() attaching the index values
+
+studentss = ["Arav" , "sneha" , "Rohan" , "abhi"]
+for i , name in enumerate(studentss, start=1):
+    print(f"{i} . {name}")
+    
+#zip
+
+names = ["Arav" , "sneha" , "Rohan" , "abhi"]
+marks_maths = [88 , 98 , 45 , 87]
+
+for name,score in zip(names, marks_maths):
+    grade = "A" if score >=90 else "B" if score>=75 else "C"
+    print(f"{name}-> {score} Grade : {grade}")
+    
+# While loop + break + continue
+data = [10 , 5 , 0 , 22 , 3 , 15 , 99]
+index = 0
+
+while index < len(data):
+    value = data[index]
+    index +=1
+    if value < 0:
+        continue
+    if value >35:
+        break
+    print(f"Processing : {value}")
+    
+# Restaurant bill using zip and for loop
+items = ["Burger" , "Pizza" , "Sandwich" ,"donuts"]
+prices = [240 , 200 , 150 , 100]
+for item,price in zip(items , prices):
+    total += price
+    gst = total *18/100
+print(f"ITEMS - {items}")
+print(f"Sub-TOTAL -> {total} \n GST - {gst}")
+final = total + gst
+print(f"YOUR TOTAL BILL IS {final}")
+
+
+# Range
+a = 9
+i = 1
+while i in range (0,11):
+    print(f"{a} x {i} = {a*i} ")
+    i += 1
+    
+# Functions
